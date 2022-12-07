5d0c6f474e4638e20759b1da79651d9c3d3f6583
desktop <hansixpog@gmail.com>
2022-12-07 18:07:27 +0700

    change getenv/environ

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -2,12 +2,11 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY', default='backend')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='backend')
 
-DEBUG = os.getenv('DEBUG', default=False)
+DEBUG = os.environ.get('DEBUG', default=False)
 
-# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='*')
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', default='*')
 
 LANGUAGE_CODE = 'ru-RU'
 
@@ -87,12 +86,12 @@ if DEBUG:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-            'NAME': os.getenv('DB_NAME', default='postgres'),
-            'USER': os.getenv('POSTGRES_USER', default='postgres'),
-            'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
-            'HOST': os.getenv('DB_HOST', default='db'),
-            'PORT': os.getenv('DB_PORT', default=5432),
+            'ENGINE': os.environ.get('DB_ENGINE', default='django.db.backends.postgresql'),
+            'NAME': os.environ.get('DB_NAME', default='postgres'),
+            'USER': os.environ.get('POSTGRES_USER', default='postgres'),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD', default='postgres'),
+            'HOST': os.environ.get('DB_HOST', default='db'),
+            'PORT': os.environ.get('DB_PORT', default=5432),
         }
     }
 
